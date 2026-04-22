8a15c39c034b60329e25532b1715aee28eb248ad
normanc0218 <149454253+normanc0218@users.noreply.github.com>
2026-04-22 21:50:33 +0000

    Implement Clerk

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,33 @@
+"use client"
 import Link from "next/link"
 import Image from "next/image"
 import NavItems from "./NavItems"
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+
 const Navbar = () => {
   return (
     <nav className='navbar'>
         <Link href='/'>
-            <div className="flex item-center gap-2.5 cursor-pointer">
+            <div className="flex items-center gap-2.5 cursor-pointer">
                 <Image 
-                    src="/images/logo.png" 
+                    src="/images/logo.svg" 
                     alt="Logo" width={46} height={44} 
                 />
             </div>     
         </Link>
         <div className="flex items-center gap-8">
             <NavItems />
-            <p>Sign In</p>
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
         </div>
     </nav>
     )
