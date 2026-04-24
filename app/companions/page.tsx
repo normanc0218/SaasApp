e1f8cdb62ab4895aff9472961d3c90ca3066f0ca
normanc0218 <149454253+normanc0218@users.noreply.github.com>
2026-04-24 17:42:08 +0000

    progress

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,6 +1,32 @@
-const CompanionsLibrary = ()=>{
+import {getAllCompanions} from "@/lib/actions/companion.actions";
+import CompanionCard from "@/components/CompanionCard";
+import {getSubjectColor} from "@/lib/utils";
+
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+    const filters = await searchParams;
+    const subject = filters.subject ? filters.subject : '';
+    const topic = filters.topic ? filters.topic : '';
+
+    const companions = await getAllCompanions({ subject, topic });
+
     return (
-        <div>Companion Library</div>
+        <main>
+            <section className="flex justify-between gap-4 max-sm:flex-col">
+                <h1>Companion Library</h1>
+                <div className="flex gap-4">
+                    Filter
+                </div>
+            </section>
+            <section className="companions-grid">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
+        </main>
     )
 }
 
